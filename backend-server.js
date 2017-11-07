2e1ba086b4880de405b948094972c55bf0132958
tomy <m.tomas.ortega@gmail.com>
2017-11-07 08:24:44 +0100

    Fix heroku deployment

--- backend-server.js
+++ backend-server.js
@@ -1,6 +1,8 @@
 let static = require('node-static');
  
 let file = new static.Server('html');
+
+let port = Number(process.env.PORT || 5000);
  
 require('http')
   .createServer((request, response) => {
@@ -10,4 +12,4 @@ require('http')
         //
         file.serve(request, response);
     }).resume();
-}).listen(5000);
+}).listen(port);
