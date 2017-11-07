2bc2f8b585c936a77650116faf83c2160fe19f49
tomy <m.tomas.ortega@gmail.com>
2017-11-07 07:50:25 +0100

    Add demo project structure

---
+++ backend-server.js
@@ -0,0 +1,13 @@
+let static = require('node-static');
+ 
+let file = new static.Server('./html');
+ 
+require('http')
+  .createServer((request, response) => {
+    request.addListener('end', () => {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
